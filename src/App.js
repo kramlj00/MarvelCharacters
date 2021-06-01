9f3aa7d0be0dca499c80aadb9f7f9d182e12f6eb
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-06-01 09:49:39 +0200

    Fetch characters and make API request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Main</h1>
-    </div>
-  );
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await axios(
+        `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b2460632688032d64ca0550d80068081&hash=a12f41440a3d2580606835c9bdde03c2`
+      );
+      console.log(result.data.data.results);
+      setCharacters(result.data.data.results);
+    };
+
+    fetch();
+  }, []);
+
+  return <h1>Main</h1>;
 }
 
 export default App;
